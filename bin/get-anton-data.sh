0484ebdfe1da5c08e12f31df1b647a77666a5eb8
andreas_kraenzle <kraenzle@k-r.ch>
2023-06-02 14:02:38 +0200

    Delete old registerdata

--- bin/get-anton-data.sh
+++ bin/get-anton-data.sh
@@ -17,7 +17,7 @@ cd $SCRIPT_DIR/../src/$locale
 for entity in ${entities[@]}; do
     ids=$(ack -oh "\bgfa-$entity-(\d+)\b" | uniq | sort | ack -oh '\d+'  | sed -e :a -e '$!N; s/\n/,/; ta')
 
-    wget --post-data "ids=$ids" $URL/$entity?api_token=$API_TOKEN -O ../register/archives-anton-$entity-$locale.xml;
+    wget --post-data "ids=$ids" $URL/$entity?api_token=$API_TOKEN -O ../register/archives-$entity-$locale.xml;
 done
 
 locale="en"
